273bd7508f87d43e64aa6815b6f3359601d659dc
Панфилова Мария Викторовна <panfilova.m.v@mac205-03.ouiit.local>
2025-02-14 14:50:42 +0300

    first commit

--- posts/admin.py
+++ posts/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 from posts.models import *
+from .models import  Author, Genre, Book
 
 admin.site.register(Tag)
 admin.site.register(Post)
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'genre', 'is_available')
+    list_filter = ('genre', 'is_checked_out')
+    search_fields = ('title', 'author__name')
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'birth_date')
+    search_fields = ('name',)
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name',)
 # Register your models here.
--- posts/models.py
+++ posts/models.py
@@ -3,6 +3,42 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField()
+    birth_date = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+class Genre(models.Model):
+    name = models.CharField(max_length=50, choices=[
+        ('художественная литература', 'Художественная литература'),
+        ('нон-фикшн', 'Нон-фикшн'),
+        ('научная фантастика', 'Научная фантастика'),
+    ])
+
+    def __str__(self):
+        return self.get_name_display()
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    isbn = models.CharField(max_length=13)
+    published_date = models.DateField()
+    genre = models.CharField(max_length=50, choices=[
+        ('художественная литература', 'Художественная литература'),
+        ('нон-фикшн', 'Нон-фикшн'),
+        ('научная фантастика', 'Научная фантастика'),
+    ])
+    is_checked_out = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+    def is_available(self):
+        return not self.is_checked_out
+
 class Tag(models.Model):
     name = models.CharField(max_length=30)
     slug = models.CharField(max_length=30)
